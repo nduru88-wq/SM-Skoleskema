58ff328459b37800e283750d88d7205130d6a42e
nduru88-wq <pmn@specialminds.dk>
2026-06-30 11:00:25 +0200

    Add service worker with caching functionality
    Implement service worker for caching files.

--- sw.js
+++ sw.js
@@ -1,1 +1,29 @@
+const CACHE = "skema-v1";
 
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+  );
+
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+
+  );
+
+});
